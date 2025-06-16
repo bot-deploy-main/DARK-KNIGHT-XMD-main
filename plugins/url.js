95adb9ef2913b9338cd99dfd8fefc42695575df3
KNIGHT-MD-V1 <botkavidu3@gmail.com>
2025-06-16 17:41:30 +0530

    Update url.js

--- plugins/url.js
+++ plugins/url.js
@@ -81,4 +81,4 @@ function formatBytes(bytes) {
   const sizes = ['Bytes', 'KB', 'MB', 'GB'];
   const i = Math.floor(Math.log(bytes) / Math.log(k));
   return parseFloat((bytes / Math.pow(k, i)).toFixed(2)) + ' ' + sizes[i];
-      }
+}
