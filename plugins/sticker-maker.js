1e29080a1c511f40c8680ad88c9a137674636394
KNIGHT-MD-V1 <botkavidu3@gmail.com>
2025-06-19 10:56:39 +0530

    Update sticker-maker.js

--- plugins/sticker-maker.js
+++ plugins/sticker-maker.js
@@ -57,7 +57,7 @@ cmd(
     async (conn, mek, m, { quoted, args, q, reply, from }) => {
         if (!mek.quoted) return reply(`*Reply to any Image or Video, Sir.*`);
         let mime = mek.quoted.mtype;
-        let pack = Config.STICKER_NAME || "Pkdriller";
+        let pack = Config.STICKER_NAME || "𝙳𝙰𝚁𝙺-𝙺𝙽𝙸𝙶𝙷𝚃-𝚇𝙼𝙳";
         
         if (mime === "imageMessage" || mime === "stickerMessage") {
             let media = await mek.quoted.download();
